2b9cd52ee788ea370c95f5f3ff0ba4d675b73af0
Nathan Perry <avaglir@gmail.com>
2018-02-02 15:49:08 -0500

    boot into long mode

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -1,4 +1,5 @@
 global start
+extern long_mode_start
 
 section .text
 bits 32
@@ -10,8 +11,13 @@ start:
     call check_cpuid
     call check_long_mode
 
-    mov dword [0xb8000], 0x2f4b2f4f
-    hlt
+    call setup_pagetable
+    call enable_paging
+
+    lgdt [gdt64.pointer] ; enable 64-bit gdt
+
+    jmp gdt64.code:long_mode_start
+
 
 check_multiboot:
     cmp eax, 0x36d76289
@@ -21,6 +27,7 @@ check_multiboot:
 .no_multiboot:
     mov al, "0"
     jmp error
+
 
 check_cpuid:
     ;; check if cpuid supported by trying to flip flag bit
@@ -47,6 +54,7 @@ check_cpuid:
     mov al, "1"
     jmp error
 
+
 check_long_mode:
     ;; check if extended processor info is available
     mov eax, 0x80000000
@@ -65,6 +73,55 @@ check_long_mode:
     mov al, "2"
     jmp error
 
+
+setup_pagetable:
+    mov eax, p3_table
+    or eax, 0b11  ; present and writable
+    mov [p4_table], eax
+
+    mov eax, p2_table
+    or eax, 0b11
+    mov [p3_table], eax
+
+    mov ecx, 0
+
+    ; map each p2 entry to a huge page
+.map_p2_table:
+    mov eax, 0x200000
+    mul ecx
+    or eax, 0b10000011 ; present, writable, huge
+    mov [p2_table + ecx * 8], eax
+
+    inc ecx
+    cmp ecx, 512
+    jne .map_p2_table
+
+    ret
+
+
+enable_paging:
+    mov eax, p4_table
+    mov cr3, eax
+
+    ; enable pae
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
+
+    ; write long mode to msr
+    mov ecx, 0xc0000080
+    rdmsr
+    or eax, 1 << 8
+    wrmsr
+
+    ; enable paging
+    mov eax, cr0
+    or eax, 1 << 31
+    mov cr0, eax
+
+    ret
+
+
 error:
     mov dword [0xb8000], 0x4f524f45
     mov dword [0xb8004], 0x4f3a4f52
@@ -72,7 +129,31 @@ error:
     mov byte  [0xb800a], al
     hlt
 
+
 section .bss
+align 4096
+
+p4_table:
+    resb 4096
+p3_table:
+    resb 4096
+p2_table:
+    resb 4096
+
 stack_bottom:
     resb 64
 stack_top:
+
+
+section .rodata
+gdt64:
+    dq 0 ; zero entry
+
+.code: equ $ - gdt64
+    ; code segment
+    ; executable, code/data desc., present, 64-bit
+    dq (1 << 43) | (1 << 44) | (1 << 47) | (1 << 53)
+
+.pointer:
+    dw $ - gdt64 - 1
+    dq gdt64
